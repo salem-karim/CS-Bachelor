8b995255e07da823ff4a4a491dd29f7d07a2b8ff
Karim Salem <147433612+salem-karim@users.noreply.github.com>
2023-10-27 06:49:08 +0200

    Übung 12

--- Semester 1/C Files/C/UE12-Pramie.c
+++ Semester 1/C Files/C/UE12-Pramie.c
@@ -2,25 +2,39 @@
 #include <stdio.h>
 
 double praemien(double betraege[], int anzahl, double grenze, double praemie){
-
+    double gesamte_praemie = 0;
+    for(int i = 0; i < anzahl; ++i){
+        if(betraege[i] >= grenze){
+            betraege[i] += praemie;
+            gesamte_praemie += praemie;
+        }    
+    }
+    return gesamte_praemie;
 }
 
 int eingabe(double betraege[], int max){
-    for(int i = 0; i < max; i++){
+    int i = 0;
+    while(i < max){
         printf("Zahl %d: ", i+1);
         scanf(" %lf", &betraege[i]);
         if(betraege[i] < 0)
             break;
-        
-        
+        ++i;
     }
+    return i;
 }
 
 double sum(double betraege[], int anzahl){
+    int i = 0;
+    double summe = 0;
+    while(i < anzahl){
+        summe += betraege[i];
+        ++i;
+    }
+    return summe;
 }
 
-int main()
-{
+int main(){
     double arr[anz] = {0};
     double grenze, praemie, gesamt;
     int n;
