299f9b58ea111bd74f1c2da8f4fb10335622e74a
Karim Salem <147433612+salem-karim@users.noreply.github.com>
2024-02-08 20:13:39 +0100

    Delete Semester 1 directory

--- Semester 1/C Files/C/UE03 Tax.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    float p;
-    scanf("%f", &p);
-    printf(": With tax: %.2f", p * 1.2);
-    
-    return 0;
-}
--- Semester 1/C Files/C/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int i = 0;
-    int* p = &i;
-    int* q = &i;
-    printf("%p\n %p\n", p, q);
-  return 0;
-}
