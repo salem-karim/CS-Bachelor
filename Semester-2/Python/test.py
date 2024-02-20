905ec2a877c459a55a3befaa3007d6f3d5b0a3cc
salem-karim <salem.karim@icloud.com>
2024-02-20 10:03:07 +0100

    made a test python file

---
+++ Semester-2/Python/test.py
@@ -0,0 +1,29 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+operation = input("Enter operation (+, -, *, /) : ")
+if operation == "+":
+    print(add(a, b))
+elif operation == "-":
+    print(subtract(a, b))
+elif operation == "*":
+    print(multiply(a, b))
+elif operation == "/":
+    print(divide(a, b))
+else:
+    print("Invalid operation")
