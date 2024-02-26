f43622fb169d9990daee040771f21047d1aa4cb0
salem-karim <salem.karim@icloud.com>
2024-02-26 23:28:19 +0100

    Made Calculator and added World Class files

---
+++ Semester-2/Websites/Taschenrechner/script.js
@@ -0,0 +1,48 @@
+const verlauf = [];
+
+function add() {
+  let a = document.getElementById("num1").value;
+  let b = document.getElementById("num2").value;
+  let result = parseFloat(a) + parseFloat(b);
+  document.getElementById("ergebnis").innerText = result;
+  verlauf.push(a + " + " + b + " = " + result);
+  showHistory();
+}
+
+function sub() {
+  let a = document.getElementById("num1").value;
+  let b = document.getElementById("num2").value;
+  let result = parseFloat(a) - parseFloat(b);
+  document.getElementById("ergebnis").innerText = result;
+  verlauf.push(a + " - " + b + " = " + result);
+  showHistory();
+}
+
+function mul() {
+  let a = document.getElementById("num1").value;
+  let b = document.getElementById("num2").value;
+  let result = parseFloat(a) * parseFloat(b);
+  document.getElementById("ergebnis").innerText = result;
+  verlauf.push(a + " * " + b + " = " + result);
+  showHistory();
+}
+
+function div() {
+  let a = document.getElementById("num1").value;
+  let b = document.getElementById("num2").value;
+  if (b == 0) {
+    result = "Division by Zero";
+  }
+  let result = parseFloat(a) / parseFloat(b);
+  document.getElementById("ergebnis").innerText = result;
+  verlauf.push(a + " / " + b + " = " + result);
+  showHistory();
+}
+
+function showHistory() {
+  const history = document.getElementById("verlauf");
+  history.innerHTML = "";
+  for (let i = 0; i < verlauf.length; i++) {
+    history.innerHTML += "<li>" + verlauf[i] + "</li>";
+  }
+}
