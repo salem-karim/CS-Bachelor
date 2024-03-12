c3242495332dc480836915f24afa622dfcba952c
salem-karim <salem.karim@icloud.com>
2024-03-12 20:48:38 +0100

    Fixed Fightclub commit

---
+++ Semester-2/C++Files/FightClub/Headers/Character.h
@@ -0,0 +1,8 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <iostream>
+
+class Character {};
+
+#endif // CHARACTER_H
---
+++ Semester-2/C++Files/FightClub/main.cpp
@@ -0,0 +1,4 @@
+#include "Headers/Character.h"
+#include <iostream>
+
+int main() { return 0; }
