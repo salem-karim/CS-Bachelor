cf56c0095eb527de95e795b69d94f1ea5ae73687
salem-karim <salem.karim@icloud.com>
2024-05-01 23:25:44 +0200

    Done Framework Übung 1

---
+++ Semester-2/Websites/WebFrameworks/WEBFR/src/app/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  email: string = 'your.mail@domain.com';
+  password: string = 'Password1239';
+}
