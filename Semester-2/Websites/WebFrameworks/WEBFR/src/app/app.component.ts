1ef115cf6d8795c7c79c0abfff6cf480a0a0b296
Karim Salem <salem.karim@icloud.com>
2024-05-15 11:13:59 +0200

    WebFr

--- Semester-2/Websites/WebFrameworks/WEBFR/src/app/app.component.ts
+++ Semester-2/Websites/WebFrameworks/WEBFR/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, SignUpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- Semester-2/Websites/WebFrameworks/WEBFR/src/app/sign-up/sign-up.component.ts
+++ Semester-2/Websites/WebFrameworks/WEBFR/src/app/sign-up/sign-up.component.ts
@@ -5,8 +5,13 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
-
+  email: string = 'a@b.com';
+  password: string = '12ab';
+  company: string = 'FH Technikum Wien';
+  street: string = 'Street';
+  city: string = 'City';
+  plz: string = '1234';
 }
