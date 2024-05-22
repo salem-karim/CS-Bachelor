eee41616b399438530cc8d579ce20661fe4e3304
Karim Salem <salem.karim@icloud.com>
2024-05-22 03:30:55 +0200

    First Step Done for WEbFR

--- Semester-2/Websites/WebFrameworks/WEBFR/src/app/sign-up/sign-up.component.ts
+++ Semester-2/Websites/WebFrameworks/WEBFR/src/app/sign-up/sign-up.component.ts
@@ -1,17 +1,52 @@
 import { Component } from '@angular/core';
+import {
+  Validators,
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
-  email: string = 'a@b.com';
-  password: string = '12ab';
+  signUpForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    password2: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
   company: string = 'FH Technikum Wien';
   street: string = 'Street';
   city: string = 'City';
   plz: string = '1234';
+
+  checkPwd() {
+    const password = this.signUpForm.get('password')?.value ?? '';
+    const password2 = this.signUpForm.get('password2')?.value ?? '';
+
+    if (password === password2) {
+      return true;
+    }
+    return false;
+  }
+
+  handleSubmit() {
+    console.log(this.signUpForm.value);
+    console.log(this.checkPwd());
+    if (this.signUpForm.valid && this.checkPwd()) {
+      console.log('Form is valid');
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
