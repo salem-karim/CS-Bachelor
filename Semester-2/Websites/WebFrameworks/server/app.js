c8698450a7083e00865d26507acb108afcdb7605
Karim Salem <salem.karim@icloud.com>
2024-06-05 08:29:41 +0200

    Express runns

--- Semester-2/Websites/WebFrameworks/server/app.js
+++ Semester-2/Websites/WebFrameworks/server/app.js
@@ -1,16 +1,26 @@
 var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
 var cors = require("cors");
 
+//Routers
 var usersRouter = require("./routes/users");
-var sessionsRouter = require("./routes/sessions");
-var highscoreRouter = require("./routes/highscore");
+var sessionRouter = require("./routes/sessions");
+var highscoreRouter = require("./routes/highscores");
 
 var app = express();
 
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 app.use("/users", usersRouter);
-app.use("/sessions", sessionsRouter);
-app.use("/highscore", highscoreRouter);
+app.use("/sessions", sessionRouter);
+app.use("/highscores", highscoreRouter);
 
 module.exports = app;
