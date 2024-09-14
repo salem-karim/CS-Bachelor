5bb2f34f699e829f2f3674b696a919c89e70c331
salem-karim <salem.karim@icloud.com>
2024-09-14 19:22:39 +0200

    used reformat code shortcut

--- Semester-3/Java/Test/src/Main.java
+++ Semester-3/Java/Test/src/Main.java
@@ -1,16 +1,18 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static String getHello() {
-        return Hello;
-    }
-    private static final String Hello = "Hello World!";
-    public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-        System.out.println(getHello());
+  public static String getHello() {
+    return Hello;
+  }
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+  private static final String Hello = "Hello World!";
+
+  public static void main(String[] args) {
+    System.out.println("Hello and welcome!");
+    System.out.println(getHello());
+
+    for (int i = 1; i <= 5; i++) {
+      System.out.println("i = " + i);
     }
+  }
 }
