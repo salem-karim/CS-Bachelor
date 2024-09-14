43af69c359ca398605bc85abc9310762241bd2e3
salem-karim <salem.karim@icloud.com>
2024-09-14 19:06:59 +0200

    some testing in Java and changing Jetbrains

--- Semester-3/Java/Test/src/Main.java
+++ Semester-3/Java/Test/src/Main.java
@@ -1,8 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static String getHello() {
+        return Hello;
+    }
+    private static final String Hello = "Hello World!";
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        System.out.println(getHello());
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
