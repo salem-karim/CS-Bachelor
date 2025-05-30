00daa1db1b785edaad9e018804ec9ff29239668b
salem-karim <salem.karim@icloud.com>
2025-05-30 21:31:08 +0200

    add SignalHandler to deno app

--- Semester-4/ITSEC/deno-app/main.ts
+++ Semester-4/ITSEC/deno-app/main.ts
@@ -1,5 +1,16 @@
 import { handleRequest } from "./routes.ts";
+import db from "./db.ts";
 
-console.log("HTTP webserver running. Access it at: http://localhost:8000/");
+const controller = new AbortController();
+const { signal } = controller;
 
-Deno.serve({ port: 8000 }, handleRequest);
+Deno.serve({ port: 8000, signal }, handleRequest);
+
+for (const signalName of ["SIGINT", "SIGTERM"] as const) {
+	Deno.addSignalListener(signalName, () => {
+		console.log(`Received ${signalName}, shutting down...`);
+		controller.abort();
+		db.close();
+		Deno.exit();
+	});
+}
