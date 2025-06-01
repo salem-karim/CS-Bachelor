10a4db44e40886f3bdd6e1611c9eb8357fb712ec
salem-karim <salem.karim@icloud.com>
2025-06-01 21:27:11 +0200

    formating

--- Semester-4/FPROG/C12/propertyBasedTests_delta.cpp
+++ Semester-4/FPROG/C12/propertyBasedTests_delta.cpp
@@ -1,8 +1,8 @@
 #include <algorithm>
-#include <random>
 #include <functional>
-#include <numeric>
 #include <limits>
+#include <numeric>
+#include <random>
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
@@ -10,99 +10,107 @@ using namespace std;
 using namespace std::placeholders;
 using namespace std::chrono;
 
-auto range = [](const int minValue, const int maxValue){
-    vector<int> range(maxValue - minValue + 1);
-    iota(range.begin(), range.end(), minValue);
-    return range;
+auto range = [](const int minValue, const int maxValue) {
+  vector<int> range(maxValue - minValue + 1);
+  iota(range.begin(), range.end(), minValue);
+  return range;
 };
 
-template<typename DestinationType>
-auto transformAll = [](const auto& source, const auto& fn){
-    DestinationType result;
-    result.resize(source.size());
-    transform(source.begin(), source.end(), result.begin(), fn);
-    return result;
+template <typename DestinationType>
+auto transformAll = [](const auto &source, const auto &fn) {
+  DestinationType result;
+  result.resize(source.size());
+  transform(source.begin(), source.end(), result.begin(), fn);
+  return result;
 };
 
-auto power = [](const int first, const int second){
-    return pow(first, second);
+auto power = [](const int first, const int second) {
+  return pow(first, second);
 };
 
-auto generate_ints = [](const int min, const int max){
-    if(min > max) {
-        return vector<int>();
-    }
+auto generate_ints = [](const int min, const int max) {
+  if (min > max) {
+    return vector<int>();
+  }
 
-    if(min == max){
-        return range(min, min);
-    }
+  if (min == max) {
+    return range(min, min);
+  }
 
-    if(max - min <= 100){
-        return range(min, max);
-    }
+  if (max - min <= 100) {
+    return range(min, max);
+  }
 
-    random_device rd;					// use for generating the seed
-    mt19937 generator(rd());	// used for generating pseudo-random numbers
-    uniform_int_distribution<int> distribution(min, max); // used to generate uniformly distributed numbers between min and max
-    auto values = transformAll<vector<int>>(range(0, 98), // generates the range [0..98] 
-            [&distribution, &generator](auto){
-                return distribution(generator); // generate the random numbers
-            });
-    values.push_back(min); // ensure that min and max values are included
-    values.push_back(max);
-    return values;
+  random_device rd;        // use for generating the seed
+  mt19937 generator(rd()); // used for generating pseudo-random numbers
+  uniform_int_distribution<int> distribution(
+      min, max); // used to generate uniformly distributed numbers between min
+                 // and max
+  auto values = transformAll<vector<int>>(
+      range(0, 98), // generates the range [0..98]
+      [&distribution, &generator](auto) {
+        return distribution(generator); // generate the random numbers
+      });
+  values.push_back(min); // ensure that min and max values are included
+  values.push_back(max);
+  return values;
 };
 
-auto generate_ints_greater_than_1 = bind(generate_ints, 1, numeric_limits<int>::max());
-auto generate_ints_greater_than_0 = bind(generate_ints, 0, numeric_limits<int>::max());
+auto generate_ints_greater_than_1 =
+    bind(generate_ints, 1, numeric_limits<int>::max());
+auto generate_ints_greater_than_0 =
+    bind(generate_ints, 0, numeric_limits<int>::max());
 
-auto all_of_collection = [](const auto& collection, const auto& fn){
-    return all_of(collection.begin(), collection.end(), fn);
+auto all_of_collection = [](const auto &collection, const auto &fn) {
+  return all_of(collection.begin(), collection.end(), fn);
 };
 
-auto printGeneratedValues = [](const string& generatorName, const auto& values){
-    cout << "Check generator " << generatorName << endl;
-    for_each(values.begin(), values.end(), [](auto value) { cout << value << ", ";});
-    cout << endl;
- };
-
-auto check_property = [](const auto& generator, const auto& property, const string& generatorName){
-    auto values = generator();
-    printGeneratedValues(generatorName, values); // should be commented finall
-    bool pass = all_of_collection(values, property);
-    CHECK(pass);
-    return pass;
+auto printGeneratedValues = [](const string &generatorName,
+                               const auto &values) {
+  cout << "Check generator " << generatorName << endl;
+  for_each(values.begin(), values.end(),
+           [](auto value) { cout << value << ", "; });
+  cout << endl;
 };
 
-auto property_0_to_power_0_is_1 = [](){
-    return power(0, 0) == 1;
+auto check_property = [](const auto &generator, const auto &property,
+                         const string &generatorName) {
+  auto values = generator();
+  printGeneratedValues(generatorName, values); // should be commented final
+  bool pass = all_of_collection(values, property);
+  CHECK(pass);
+  return pass;
 };
 
-auto prop_0_to_any_nonzero_int_is_0= [](const int exponent){
-    CHECK(exponent > 0);
-    return power(0, exponent) == 0;
+auto property_0_to_power_0_is_1 = []() { return power(0, 0) == 1; };
+
+auto prop_0_to_any_nonzero_int_is_0 = [](const int exponent) {
+  CHECK(exponent > 0);
+  return power(0, exponent) == 0;
 };
 
-auto prop_anyIntToPower0Is1 = [](const int base){
-    CHECK(base > 0);
-    return power(base, 0) == 1;
+auto prop_anyIntToPower0Is1 = [](const int base) {
+  CHECK(base > 0);
+  return power(base, 0) == 1;
 };
 
-auto prop_any_int_to_power_1_is_the_value = [](const int base){
-    return power(base, 1) == base;
+auto prop_any_int_to_power_1_is_the_value = [](const int base) {
+  return power(base, 1) == base;
 };
 
-TEST_CASE("Properties"){
-    cout << "Property: 0 to power 0 is 1" << endl;
-    CHECK(property_0_to_power_0_is_1);
+TEST_CASE("Properties") {
+  cout << "Property: 0 to power 0 is 1" << endl;
+  CHECK(property_0_to_power_0_is_1);
 
-    cout << "Property: 0 to any non-zero power is 0" << endl;
-    check_property(generate_ints_greater_than_1, prop_0_to_any_nonzero_int_is_0, "generate ints");
+  cout << "Property: 0 to any non-zero power is 0" << endl;
+  check_property(generate_ints_greater_than_1, prop_0_to_any_nonzero_int_is_0,
+                 "generate ints");
 
-    cout << "Property: any int to power 0 is 1" << endl;
-    check_property(generate_ints_greater_than_1, prop_anyIntToPower0Is1, "generate ints");
+  cout << "Property: any int to power 0 is 1" << endl;
+  check_property(generate_ints_greater_than_1, prop_anyIntToPower0Is1,
+                 "generate ints");
 
-    cout << "Property: any int to power 1 is the value" << endl;
-    check_property(generate_ints_greater_than_0, prop_any_int_to_power_1_is_the_value, "generate ints");
-
+  cout << "Property: any int to power 1 is the value" << endl;
+  check_property(generate_ints_greater_than_0,
+                 prop_any_int_to_power_1_is_the_value, "generate ints");
 }
