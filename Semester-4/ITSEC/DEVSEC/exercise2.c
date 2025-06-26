ec26baf50e79c1a54ea91855b99a24cbbf47adf4
salem-karim <salem.karim@icloud.com>
2025-06-26 14:00:20 +0200

    fix: only comment the error don't fix them

--- Semester-4/ITSEC/DEVSEC/exercise2.c
+++ Semester-4/ITSEC/DEVSEC/exercise2.c
@@ -1,91 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
-
-#define BUF_LEN 50
-#define MAX_USERS 5
-#define true 1
 
 // Dummy-Database for users (in case of a real)
 // This is not an failure it is just to make the exercise a little bit easier
 // for you
-typedef struct {
-  char username[BUF_LEN];
-  char password[BUF_LEN];
-  char sessionID[BUF_LEN];
-  uint isAdmin;
-} User;
+struct User {
+  char username[50];
+  char password[50];
+  char sessionID[50];
+  int isAdmin;
+};
+
+#define MAX_USERS 5
+struct User users[MAX_USERS];
+int userCount = 0;
 
 // Function to register users
-void registerUser(User users[MAX_USERS], int *userCount) {
-  char username[BUF_LEN];
-  char password[BUF_LEN];
+void registerUser() {
+  char username[50];
+  char password[50];
 
   // BUG: CWE-306: Missing Authentication for Critical Function
-  // If user already has a session return
 
   printf("Enter a username: ");
   // BUG: CWE-120 Bufferoverflow possible
-  // scanf("%s", username);
-
-  // scanf("%49s", username); or better
-  fgets(username, sizeof(username), stdin);
+  scanf("%s", username);
 
   printf("Enter a password: ");
   // BUG: CWE-120 Bufferoverflow possible
-  // scanf("%s", password);
-
-  // scanf("%49s", password); or better
-  fgets(password, sizeof(password), stdin);
-
-  // TODO: Add in check for safe Password CWE-521
+  scanf("%s", password);
+  // BUG: CWE-521: Weak Password Requirements
 
   // BUG: CWE-120 Bufferoverflow possible
-  // strcpy(users[userCount].username, username);
-  strncpy(users[*userCount].username, username, sizeof(username));
+  strcpy(users[userCount].username, username);
   // BUG: CWE-120 Bufferoverflow possible
-  // strcpy(users[userCount].password, password);
-  strncpy(users[*userCount].password, password, sizeof(password));
-  users[*userCount].isAdmin = 0;
+  strcpy(users[userCount].password, password);
+  users[userCount].isAdmin = 0;
   userCount++;
 
   printf("User %s registered successfully!\n", username);
 }
 
-// Function for login
-void loginUser(User users[MAX_USERS], const int userCount) {
-  char username[BUF_LEN];
-  char password[BUF_LEN];
+// Funktion for login
+void loginUser() {
+  char username[50];
+  char password[50];
 
   // BUG: CWE-306: Missing Authentication for Critical Function
-  // If user already has a session return
 
+  // BUG: CWE-307: Improper Restriction of Excessive Authentication Attempts
   while (1) {
     printf("Enter your username: ");
-
     // BUG: CWE-120 Bufferoverflow possible
-    // scanf("%s", username);
-
-    // scanf("%49s", username); or better
-    fgets(username, sizeof(username), stdin);
-
+    scanf("%s", username);
     printf("Enter your password: ");
-
     // BUG: CWE-120 Bufferoverflow possible
-    // scanf("%s", password);
+    scanf("%s", password);
 
-    // scanf("%49s", password); or better
-    fgets(password, sizeof(password), stdin);
-
-    // BUG: CWE-307: Improper Restriction of Excessive Authentication Attempts
     for (int i = 0; i < userCount; i++) {
       if (strcmp(users[i].username, username) == 0 &&
           strcmp(users[i].password, password) == 0) {
+        // BUG: CWE-120 Bufferoverflow possible ?
         printf("Login successful! Welcome, %s\n", username);
 
-        // BUG: CWE-613: Insufficient Session Expiration
+        // BUG: CWE-613: Insufficient Session Expiration ?
         // BUG: CWE-384: Session Fixation
+        // bigger then 50
         strcpy(users[i].sessionID, "123456");
         return;
       }
@@ -96,26 +77,29 @@ void loginUser(User users[MAX_USERS], const int userCount) {
 }
 
 // Function for password reset
-void resetPassword(User users[MAX_USERS], const int userCount) {
-  char username[BUF_LEN];
-  char securityAnswer[BUF_LEN];
-  char newPassword[BUF_LEN];
-
-  // BUG: CWE-306: Missing Authentication for Critical Function
+void resetPassword() {
+  char username[50];
+  char securityAnswer[50];
+  char newPassword[50];
 
   printf("Enter your username: ");
+  // BUG: CWE-120 Bufferoverflow possible
   scanf("%s", username);
 
   // BUG: CWE-640: Weak Password Recovery Mechanism for Forgotten Password
   printf("What is your favorite color? ");
+  // BUG: CWE-120 Bufferoverflow possible
   scanf("%s", securityAnswer);
 
   if (strcmp(securityAnswer, "blue") == 0) {
     printf("Enter your new password: ");
+    // BUG: CWE-120 Bufferoverflow possible
     scanf("%s", newPassword);
+    // BUG: CWE-521: Weak Password Requirements
 
     for (int i = 0; i < userCount; i++) {
       if (strcmp(users[i].username, username) == 0) {
+        // BUG: CWE-120 Bufferoverflow possible
         strcpy(users[i].password, newPassword);
         printf("Password reset successful for user %s.\n", username);
         return;
@@ -130,10 +114,11 @@ void resetPassword(User users[MAX_USERS], const int userCount) {
 void uploadFile() {
   // BUG: CWE-306: Missing Authentication for Critical Function
 
-  char filename[BUF_LEN * 2];
+  char filename[100];
   printf("Enter the filename to upload: ");
-  // BUG: CWE-434: Unrestricted Upload of File with Dangerous Type
+  // BUG: CWE-120 Bufferoverflow possible
   scanf("%s", filename);
+  // BUG: CWE-434: Unrestricted Upload of File with Dangerous Type
 
   FILE *file = fopen(filename, "w");
   if (file == NULL) {
@@ -150,39 +135,34 @@ void uploadFile() {
 
 // Main Function
 int main() {
-  int choice = 0;
-  User users[MAX_USERS];
-  memset(users, 0, sizeof(users));
-  int userCount = 0;
+  int choice;
 
-  enum { REGISTER = 1, LOGIN, RESETPASSWD, UPLOADFILE, EXIT };
-
-  while (true) {
+  while (1) {
     printf("\n1. Register\n2. Login\n3. Reset Password\n4. Upload File\n5. "
            "Exit\n");
     printf("Choose an option: ");
-    // BUG: UNSAFE
+    // BUG: CWE-120 Bufferoverflow possible
     scanf("%d", &choice);
 
     switch (choice) {
-    case REGISTER:
-      registerUser(users, &userCount);
+    case 1:
+      registerUser();
       break;
-    case LOGIN:
-      loginUser(users, userCount);
+    case 2:
+      loginUser();
       break;
-    case RESETPASSWD:
-      resetPassword(users, userCount);
+    case 3:
+      resetPassword();
       break;
-    case UPLOADFILE:
+    case 4:
       uploadFile();
       break;
-    case EXIT:
-      exit(EXIT_SUCCESS);
+    case 5:
+      exit(0);
     default:
       printf("Invalid choice.\n");
     }
   }
 
-  return EXIT_SUCCESS;
+  return 0;
 }
